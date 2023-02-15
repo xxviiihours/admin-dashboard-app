b4e188bb74faf49b7c3d0291856de66c5b3255fa
Kevz Macalalag <mc.kvnflrs@gmail.com>
2023-02-15 09:35:38 +0800

    Added FAQ Mockup Page

--- admin-ui/src/layout/TheContent.jsx
+++ admin-ui/src/layout/TheContent.jsx
@@ -7,10 +7,10 @@ const Contact = React.lazy(() => import("./../views/contact"));
 const Invoice = React.lazy(() => import("./../views/invoice"));
 const Form = React.lazy(() => import("./../views/form"));
 const Calendar = React.lazy(() => import("./../views/calendar"));
+const FAQ = React.lazy(() => import("./../views/faq"));
 // const Bar = React.lazy(() => import("./../views/bar"));
 // const Pie = React.lazy(() => import("./../views/pie"));
 // const Line = React.lazy(() => import("./../views/line"));
-// const FAQ = React.lazy(() => import("./../views/faq"));
 // const Geography = React.lazy(() => import("./../views/geography"));
 // const Calendar = React.lazy(() => import("./../views/calendar"));
 
@@ -23,10 +23,10 @@ const TheContent = () => {
 			<Route path='/invoices' element={<Invoice />} />
 			<Route path='/create-member' element={<Form />} />
 			<Route path='/calendar' element={<Calendar />} />
+			<Route path='/faq' element={<FAQ />} />
 			{/* <Route path='/bar' element={<Bar />} /> */}
 			{/* <Route path='/pie' element={<Pie />} /> */}
 			{/* <Route path='/line' element={<Line />} /> */}
-			{/* <Route path='/faq' element={<FAQ />} /> */}
 			{/* <Route path='/geography' element={<Geography />} /> */}
 			{/* <Route path='/calendar' element={<Calendar />} /> */}
 		</Routes>
