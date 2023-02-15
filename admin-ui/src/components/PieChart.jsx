418f13b2835c15246715102e7456ae0a5ad9100b
Kevz Macalalag <mc.kvnflrs@gmail.com>
2023-02-15 11:45:22 +0800

    modified:   admin-ui/src/components/PieChart.jsx

--- admin-ui/src/components/PieChart.jsx
+++ admin-ui/src/components/PieChart.jsx
@@ -85,56 +85,6 @@ const PieChart = ({ isDashboard = false }) => {
 					spacing: 10,
 				},
 			]}
-			fill={[
-				{
-					match: {
-						id: 'ruby',
-					},
-					id: 'dots',
-				},
-				{
-					match: {
-						id: 'c',
-					},
-					id: 'dots',
-				},
-				{
-					match: {
-						id: 'go',
-					},
-					id: 'dots',
-				},
-				{
-					match: {
-						id: 'python',
-					},
-					id: 'dots',
-				},
-				{
-					match: {
-						id: 'scala',
-					},
-					id: 'lines',
-				},
-				{
-					match: {
-						id: 'lisp',
-					},
-					id: 'lines',
-				},
-				{
-					match: {
-						id: 'elixir',
-					},
-					id: 'lines',
-				},
-				{
-					match: {
-						id: 'javascript',
-					},
-					id: 'lines',
-				},
-			]}
 			legends={[
 				{
 					anchor: 'bottom',
