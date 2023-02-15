084c5e22fa65c82ae0aa78eecc5be316eb9471dd
Kevz Macalalag <mc.kvnflrs@gmail.com>
2023-02-15 17:52:19 +0800

    Fixed Geography Title Page

--- admin-ui/src/views/Geography/index.jsx
+++ admin-ui/src/views/Geography/index.jsx
@@ -10,7 +10,7 @@ const Geography = () => {
 
   return (
     <Box m={'20px'}>
-			<ContentHeader title={'Line Chart'} subtitle={'Simple Line Chart'} />
+			<ContentHeader title={'Geography Chart'} subtitle={'Simple Geography Chart'} />
       <Box height={"75vh"} border={`1px solid ${colors.grey[100]}`} borderRadius={'4px'}>
         <GeographyChart />
       </Box>
